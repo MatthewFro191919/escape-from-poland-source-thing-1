d00e63498de4721073ac2fbdabf32d25f3d0fd6b
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-06-29 09:36:33 -0400

    Update AnimateAtlas.hx

--- source/util/AnimateAtlas.hx
+++ source/util/AnimateAtlas.hx
@@ -6,8 +6,8 @@ import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.frames.FlxFrame;
 import flixel.graphics.frames.FlxFramesCollection;
+import flixel.math.FlxRect;
 import haxe.Json;
-import openfl.geom.Rectangle;
 import openfl.geom.Point;
 
 class AnimateAtlas
@@ -19,36 +19,38 @@ class AnimateAtlas
     ):FlxAtlasFrames
     {
         var frames = new FlxAtlasFrames(image);
-        var spriteMap = spritemapData.ATLAS.SPRITES;
+        var spriteMap:Array<Dynamic> = cast spritemapData.ATLAS.SPRITES;
         var spriteIndex = new Map<String, FlxFrame>();
 
         // Add all frames from the spritemap
         for (entry in spriteMap)
         {
             var spr = entry.SPRITE;
-            var rect = new Rectangle(spr.x, spr.y, spr.w, spr.h);
+            var rect = new FlxRect(spr.x, spr.y, spr.w, spr.h);
             var name = spr.name + ".png";
             var frame = frames.addAtlasFrame(rect, new Point(0, 0), name);
             spriteIndex.set(spr.name, frame);
         }
 
         // Auto-detect and map animations from the top-level timeline
-        var topLayer = animationData.AN.TL.L[0];
+        var topLayer:Dynamic = animationData.AN.TL.L[0];
         var anims:Map<String, Array<String>> = new Map();
 
-        for (frame in topLayer.FR)
+        var frameList:Array<Dynamic> = cast topLayer.FR;
+        for (frame in frameList)
         {
-            for (element in frame.E)
+            var elements:Array<Dynamic> = cast frame.E;
+            for (element in elements)
             {
-                var symbolPath = element.SI.SN;
+                var symbolPath:String = element.SI.SN;
                 var tag = symbolPath.split("/").pop();
                 if (!anims.exists(tag)) anims.set(tag, []);
                 anims.get(tag).push(tag);
             }
         }
 
-        // Also include Symbol Definitions if present (for parts like Idle-Forward, etc.)
-        var symbols = animationData.SD.S;
+        // Also include Symbol Definitions if present
+        var symbols:Array<Dynamic> = cast animationData.SD.S;
         for (symbol in symbols)
         {
             var name:String = symbol.SN.split("/").pop();
@@ -56,12 +58,14 @@ class AnimateAtlas
 
             if (symbol.TL != null && symbol.TL.L != null)
             {
-                var layers = symbol.TL.L;
+                var layers:Array<Dynamic> = cast symbol.TL.L;
                 for (layer in layers)
                 {
-                    for (frame in layer.FR)
+                    var layerFrames:Array<Dynamic> = cast layer.FR;
+                    for (frame in layerFrames)
                     {
-                        for (element in frame.E)
+                        var elements:Array<Dynamic> = cast frame.E;
+                        for (element in elements)
                         {
                             var frameName = null;
                             if (element.SI != null)
@@ -85,8 +89,7 @@ class AnimateAtlas
 
             if (flxFrames.length > 0)
             {
-                // Auto-set FPS and loop if needed later
-                frames.addSpriteSheetAnimation(name, flxFrames, 24);
+                frames.frames[name] = flxFrames;
             }
         }
 
