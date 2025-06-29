1de4067ec23c5d359a39b3df878498e879dfb12d
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-06-29 09:46:45 -0400

    Update AnimateAtlas.hx

--- source/util/AnimateAtlas.hx
+++ source/util/AnimateAtlas.hx
@@ -7,77 +7,77 @@ import flixel.graphics.frames.FlxAtlasFrames;
 import flixel.graphics.frames.FlxFrame;
 import flixel.graphics.frames.FlxFramesCollection;
 import flixel.math.FlxRect;
+import flixel.math.FlxPoint;
 import haxe.Json;
-import openfl.geom.Point;
 
-class AnimateAtlas {
-	public static function buildFrames(image:FlxGraphic, spritemapData:Dynamic, animationData:Dynamic):FlxAtlasFrames {
-		var frames = new FlxAtlasFrames(image);
-		var spriteMap:Array<Dynamic> = cast spritemapData.ATLAS.SPRITES;
-		var spriteIndex = new Map<String, FlxFrame>();
+class AnimateAtlas
+{
+    public static function buildFrames(
+        image:FlxGraphic,
+        spritemapData:Dynamic,
+        animationData:Dynamic
+    ):FlxAtlasFrames
+    {
+        var frames = new FlxAtlasFrames(image);
+        var spriteMap:Array<Dynamic> = cast spritemapData.ATLAS.SPRITES;
+        var spriteIndex = new Map<String, FlxFrame>();
 
-		// Add all frames from the spritemap
-		for (entry in spriteMap) {
-			var spr = entry.SPRITE;
-			var rect = new FlxRect(spr.x, spr.y, spr.w, spr.h);
-			var name = spr.name + ".png";
-			var frame = frames.addAtlasFrame(rect, new Point(0, 0), name);
-			spriteIndex.set(spr.name, frame);
-		}
+        // Add all frames from the spritemap
+        for (entry in spriteMap)
+        {
+            var spr = entry.SPRITE;
+            var rect = new FlxRect(spr.x, spr.y, spr.w, spr.h);
+            var name = spr.name + ".png";
+            var frame = frames.addAtlasFrame(rect, new FlxPoint(0, 0), new FlxPoint(spr.w, spr.h));
+            frame.name = name;
+            spriteIndex.set(spr.name, frame);
+        }
 
-		// Auto-detect and map animations from the top-level timeline
-		var topLayer:Dynamic = animationData.AN.TL.L[0];
-		var anims:Map<String, Array<String>> = new Map();
+        // Also include Symbol Definitions if present
+        var symbols:Array<Dynamic> = cast animationData.SD.S;
+        for (symbol in symbols)
+        {
+            var name:String = symbol.SN.split("/").pop();
+            var symbolFrames:Array<String> = [];
 
-		var frameList:Array<Dynamic> = cast topLayer.FR;
-		for (frame in frameList) {
-			var elements:Array<Dynamic> = cast frame.E;
-			for (element in elements) {
-				var symbolPath:String = element.SI.SN;
-				var tag = symbolPath.split("/").pop();
-				if (!anims.exists(tag))
-					anims.set(tag, []);
-				anims.get(tag).push(tag);
-			}
-		}
+            if (symbol.TL != null && symbol.TL.L != null)
+            {
+                var layers:Array<Dynamic> = cast symbol.TL.L;
+                for (layer in layers)
+                {
+                    var layerFrames:Array<Dynamic> = cast layer.FR;
+                    for (frame in layerFrames)
+                    {
+                        var elements:Array<Dynamic> = cast frame.E;
+                        for (element in elements)
+                        {
+                            var frameName = null;
+                            if (element.SI != null)
+                                frameName = element.SI.SN.split("/").pop();
+                            else if (element.ASI != null)
+                                frameName = element.ASI.N;
 
-		// Also include Symbol Definitions if present
-		var symbols:Array<Dynamic> = cast animationData.SD.S;
-		for (symbol in symbols) {
-			var name:String = symbol.SN.split("/").pop();
-			var symbolFrames:Array<String> = [];
+                            if (frameName != null)
+                                symbolFrames.push(frameName);
+                        }
+                    }
+                }
+            }
 
-			if (symbol.TL != null && symbol.TL.L != null) {
-				var layers:Array<Dynamic> = cast symbol.TL.L;
-				for (layer in layers) {
-					var layerFrames:Array<Dynamic> = cast layer.FR;
-					for (frame in layerFrames) {
-						var elements:Array<Dynamic> = cast frame.E;
-						for (element in elements) {
-							var frameName = null;
-							if (element.SI != null)
-								frameName = element.SI.SN.split("/").pop();
-							else if (element.ASI != null)
-								frameName = element.ASI.N;
+            var flxFrames:Array<FlxFrame> = [];
+            for (frameName in symbolFrames)
+            {
+                if (spriteIndex.exists(frameName))
+                    flxFrames.push(spriteIndex.get(frameName));
+            }
 
-							if (frameName != null)
-								symbolFrames.push(frameName);
-						}
-					}
-				}
-			}
+            // Add each frame individually into the FlxAtlasFrames collection
+            for (i in 0...flxFrames.length)
+            {
+                frames.pushFrame(flxFrames[i]);
+            }
+        }
 
-			var flxFrames:Array<FlxFrame> = [];
-			for (frameName in symbolFrames) {
-				if (spriteIndex.exists(frameName))
-					flxFrames.push(spriteIndex.get(frameName));
-			}
-
-			if (flxFrames.length > 0) {
-				frames.frames[name] = flxFrames;
-			}
-		}
-
-		return frames;
-	}
+        return frames;
+    }
 }
