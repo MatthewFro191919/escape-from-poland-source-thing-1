96dfe5fe3abb3568e7e6addf45f6ae107781254a
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-07-13 19:41:58 -0400

    Update Bulid.bat

--- Bulid.bat
+++ Bulid.bat
@@ -37,6 +37,22 @@ haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
 haxelib remove flixel
+haxelib remove lime
+haxelib remove openfl
+haxelib remove flixel-ui
+haxelib remove hxCodec
+haxelib remove actuate
+haxelib remove flixel-addons
+haxelib remove flixel-tools
 haxelib install flixel 4.11.0
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.0
+haxelib install flixel-tools
+haxelib install flixel-ui
+haxelib install flixel-addons 2.9.0
+haxelib install actuate 1.9.0
+haxelib install hxCodec 2.5.1
+haxelib run lime setup flixel
+haxelib run lime setup
 haxelib list
 echo Finished!
