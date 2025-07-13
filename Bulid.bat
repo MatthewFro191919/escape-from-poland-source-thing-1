77386914e0d07eba1a9d83a414dfa0e9cd856a91
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-07-13 19:35:29 -0400

    Update Bulid.bat

--- Bulid.bat
+++ Bulid.bat
@@ -3,14 +3,14 @@ title Setup - PECG
 cd ..
 echo Installing dependencies, please wait...
 haxelib install hxcpp
-haxelib install lime 8.0.0
-haxelib install openfl 9.3.3
+haxelib install lime 8.1.2
+haxelib install openfl 9.3.0
 haxelib install flixel 4.11.0
 haxelib run lime setup flixel
 haxelib run lime setup         
 haxelib remove flixel-addons
 haxelib remove flixel-tools
-haxelib remove flixel-ui
+haxelib install flixel-ui 2.6.1
 haxelib install flixel-tools
 haxelib install flixel-ui
 haxelib install flixel-addons 2.9.0
@@ -25,7 +25,7 @@ haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 haxelib git faxe https://github.com/uhrobots/faxe
 haxelib git polymod https://github.com/larsiusprime/polymod.git
 haxelib install hxcpp-debug-server
-haxelib install actuate 1.9.0 
+haxelib install actuate 1.9.0
 haxelib install hxCodec 2.5.1
 haxelib install linc_luajit
 haxelib install hscript
