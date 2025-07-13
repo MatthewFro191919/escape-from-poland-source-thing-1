c4ed7a53a04d6e5be1b8a21affbe58edd7bbd02d
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-07-13 19:53:44 -0400

    Update Bulid.bat

--- Bulid.bat
+++ Bulid.bat
@@ -1,58 +1,21 @@
 @echo off
 title Setup - PECG
 cd ..
+
 echo Installing dependencies, please wait...
-haxelib install hxcpp
 haxelib install lime 8.1.2
 haxelib install openfl 9.3.3
 haxelib install flixel 4.11.0
-haxelib run lime setup flixel
-haxelib run lime setup         
-haxelib remove flixel-addons
-haxelib remove flixel-tools
+haxelib install flixel-addons 2.11.0
+haxelib install flixel-tools
 haxelib install flixel-ui 2.6.1
-haxelib install flixel-tools
-haxelib install flixel-ui
-haxelib install flixel-addons 2.9.0
-haxelib install tjson
-haxelib install hxjsonast
-haxelib install hxCodec 2.5.1
-haxelib install linc_luajit
+haxelib install actuate 1.9.0 
 haxelib install hscript
-haxelib install openfl-webm
-haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
-haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
-haxelib git faxe https://github.com/uhrobots/faxe
-haxelib git polymod https://github.com/larsiusprime/polymod.git
-haxelib install hxcpp-debug-server
-haxelib install actuate 1.9.0
-haxelib install hxCodec 2.5.1
+haxelib install hxCodec 2.5.1          
 haxelib install linc_luajit
-haxelib install hscript
-haxelib install newgrounds
-haxelib install extension-webm
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate 768740a56b26aa0c072720e0d1236b94afe68e3e
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
-haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
-haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
-haxelib remove flixel
-haxelib remove lime
-haxelib remove openfl
-haxelib remove flixel-ui
-haxelib remove hxCodec
-haxelib remove actuate
-haxelib remove flixel-addons
-haxelib remove flixel-tools
-haxelib install flixel 4.11.0
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
-haxelib install flixel-tools
-haxelib install flixel-ui
-haxelib install flixel-addons 2.9.0
-haxelib install actuate 1.9.0
-haxelib install hxCodec 2.5.1
-haxelib run lime setup flixel
-haxelib run lime setup
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate
+haxelib install hxcpp-debug-server
 haxelib list
-echo Finished!
