a89ace3e6ff57c7b7853dce94c249ff2e74e190a
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-07-13 20:21:00 -0400

    Update Bulid.bat

--- Bulid.bat
+++ Bulid.bat
@@ -13,7 +13,9 @@ haxelib install actuate 1.9.0
 haxelib install hscript
 haxelib install hxCodec 2.5.1          
 haxelib install linc_luajit
+haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
 haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate
 haxelib install hxcpp-debug-server
 haxelib list
