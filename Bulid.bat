36d33e993cad653f4f779c4f66f6640607a9e78a
MatthewFro191919 <matthewfroncek4@gmail.com>
2025-07-13 20:10:54 -0400

    Update Bulid.bat

--- Bulid.bat
+++ Bulid.bat
@@ -3,19 +3,28 @@ title Setup - PECG
 cd ..
 
 echo Installing dependencies, please wait...
-haxelib install lime 8.1.2
-haxelib install openfl 9.3.3
+haxelib setup C:/haxelib
+haxelib install hxcpp > nul
+haxelib install lime 8.0.0
+haxelib install openfl
 haxelib install flixel 4.11.0
-haxelib install flixel-addons 2.11.0
+haxelib run lime setup flixel
+haxelib run lime setup         
+haxelib remove flixel-addons
+haxelib remove flixel-tools
+haxelib remove flixel-ui
 haxelib install flixel-tools
-haxelib install flixel-ui 2.6.1
-haxelib install actuate 1.9.0 
-haxelib install hscript
-haxelib install hxCodec 2.5.1          
+haxelib install flixel-ui
+haxelib install flixel-addons 2.9.0
+haxelib install tjson
+haxelib install hxjsonast
+haxelib install hxCodec 2.5.1
 haxelib install linc_luajit
-haxelib git flxanimate https://github.com/ShadowMario/flxanimate dev
+haxelib install hscript
+haxelib git hscript-ex https://github.com/ianharrigan/hscript-ex
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
 haxelib git faxe https://github.com/uhrobots/faxe
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate
+haxelib git polymod https://github.com/larsiusprime/polymod.git
 haxelib install hxcpp-debug-server
 haxelib list
